a6bfbd85cefe2ee7ab8dc7d5a7c6f770fc050d8f
tenajuro12 <221939@astanait.edu.kz>
2024-02-22 16:22:31 +0600

    first commit

--- routes/route.go
+++ routes/route.go
@@ -7,9 +7,19 @@ import (
 )
 
 func Setup(app *fiber.App) {
-	app.Post("/api/register",controller.Register)
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", "*")
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Method() == "OPTIONS" {
+			return c.SendStatus(fiber.StatusOK)
+		}
+		return c.Next()
+	})
+
+	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
-	
+
 	app.Use(middleware.IsAuthenticate)
 	app.Post("/api/post", controller.CreatePost)
 	app.Get("/api/allpost", controller.AllPost)
@@ -19,6 +29,4 @@ func Setup(app *fiber.App) {
 	app.Delete("/api/deletepost/:id", controller.DeletePost)
 	app.Post("/api/upload-image", controller.Upload)
 	app.Static("/api/uploads", "./uploads")
-	 
-	
 }
