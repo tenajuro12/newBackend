825dbc2dcd9fac47022c9da09fc2bf4686f63c8f
tenajuro12 <221939@astanait.edu.kz>
2024-02-22 17:10:35 +0600

    first commit

--- routes/route.go
+++ routes/route.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/kingztech2019/blogbackend/controller"
+	"github.com/kingztech2019/blogbackend/middleware"
 )
 
 func Setup(app *fiber.App) {
@@ -15,6 +16,8 @@ func Setup(app *fiber.App) {
 
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
+
+	app.Use(middleware.IsAuthenticate)
 
 	app.Post("/api/post", controller.CreatePost)
 	app.Get("/api/allpost", controller.AllPost)
