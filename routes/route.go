dfac40c2d9e01445ed54e44ffc08ae01fd12adcf
tenajuro12 <221939@astanait.edu.kz>
2024-02-22 17:31:29 +0600

    first commit

--- routes/route.go
+++ routes/route.go
@@ -2,11 +2,19 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/kingztech2019/blogbackend/controller"
 	"github.com/kingztech2019/blogbackend/middleware"
 )
 
 func Setup(app *fiber.App) {
+	// Add this line to setup CORS for your application
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000", // This is your frontend's address
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowCredentials: true,
+	}))
+
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
 
@@ -19,5 +27,4 @@ func Setup(app *fiber.App) {
 	app.Delete("/api/deletepost/:id", controller.DeletePost)
 	app.Post("/api/upload-image", controller.Upload)
 	app.Static("/api/uploads", "./uploads")
-
 }
